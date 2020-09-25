26167d16353accff5274c12d81ee20b20c37bc5d
mark <mark.miao@inspiry.cn>
2020-09-25 11:07:37 +0800

    f

---
+++ appworks/src/components/NavigationBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="navigationBarCss">
+      <div class="username">
+          xxx
+      </div>
+      <div class="navTitle">
+          ffff
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.navigationBarCss {
+  width: 100%;
+  height: 60px;
+  background-color: white;
+  left: 0;
+  top: 0;
+  display: flex;
+  box-shadow:0px 10px 8px -14px #1f1f1f;
+}
+.username {
+    /* background-color: red; */
+    /* line-height: 60px; */
+    align-self: center;
+    padding-left: 100px;
+    font-size: 30px;
+}
+.navTitle {
+    /* width: 50%; */
+    height: 60px;
+    background-color: royalblue;
+    /* flex: 1; */
+    /* text-align: right; */
+}
+</style>
---
+++ appworks/src/view/home.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div>
+      <navBar />
+  </div>
+</template>
+
+<script>
+import navBar from '@/components/NavigationBar'
+
+export default {
+  name: 'home',
+  components: {
+    navBar
+  }
+}
+</script>
+
+<style>
+</style>
